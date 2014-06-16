ea8eb2a80323c66684ce608e8c0c1113d7fb9acd
helenswanson <hhyswanson@gmail.com>
2014-06-16 11:26:09 -0400

    Added spec for seeing events listed on the events page

--- spec/features/user_views_events_listing_spec.rb
+++ spec/features/user_views_events_listing_spec.rb
@@ -14,4 +14,16 @@ feature "User visits the events page" do
     visit '/events'
     expect(page).to have_content "All The Events"
   end
+
+  it "sees all the events listed on the page" do
+    # Create two events so we can test that they're displayed on the page
+    event1 = Event.create!(location: "My Backyard")
+    event2 = Event.create!(location: "Chuckie Cheese")
+
+    visit '/events'
+
+    expect(page).to have_content event1.location
+    expect(page).to have_content event2.location
+  end
+
 end
